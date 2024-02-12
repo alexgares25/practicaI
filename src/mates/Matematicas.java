d335d3a43daebc356fc849463efc654a020c25a3
alexgares25 <72692375+alexgares25@users.noreply.github.com>
2024-02-12 11:05:24 +0100

    Update Matematicas.java

--- src/mates/Matematicas.java
+++ src/mates/Matematicas.java
@@ -1,12 +1,17 @@
-package mates;
-import java.util.Random;
-public class Matematicas{
+/**
+ * Clase Matematicas que contiene métodos relacionados con operaciones matemáticas,
+ * incluyendo la generación de una aproximación al número Pi mediante el método de Montecarlo.
+ */
+public class Matematicas {
+
     /**
-     * * Genera una aproximación al número Pi mediante el método de
-     * * Montecarlo. El parámetro `pasos` indica el número de puntos
-     * * generado.
-     * */
-        public static double generarNumeroPiIterativo(long pasos) {
+     * Genera una aproximación al número Pi mediante el método de Montecarlo.
+     * El parámetro `pasos` indica el número de puntos generados.
+     *
+     * @param pasos El número de puntos generados para la estimación de Pi.
+     * @return Una aproximación al número Pi basada en el método de Montecarlo.
+     */
+    public static double generarNumeroPiIterativo(long pasos) {
         Random random = new Random();
         long dentroCirculo = 0;
 
@@ -21,5 +26,5 @@ public class Matematicas{
         }
 
         return 4.0 * dentroCirculo / pasos;
-	}
+    }
 }
